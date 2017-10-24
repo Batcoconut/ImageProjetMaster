cf0954d30eebc39e7e054e4888c3127920015ee5
tibo <lelonthi@gmail.com>
2017-10-24 10:07:22 +0200

    DataBase + script main

--- ImageResize.py
+++ ImageResize.py
@@ -6,17 +6,17 @@ import os
 height = 1024
 
 print('list des images')
-Liste = os.listdir('../Images_dataset')
+Liste = os.listdir('./BaseT')
 
 
 for i in range (1,len(Liste)):
     print(Liste[i])
-    img = Image.open ('../Images_dataset/' +Liste[i])
+    img = Image.open ('./BaseT/' +Liste[i])
     wpercent = (height / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     newImage = img.resize((height, hsize))
     print(newImage)
-    newImage.save('./Image_data/'+ Liste[i])
+    newImage.save('./BaseTest/'+ Liste[i])
 
 
 
